7b0b87244d5c1833e8c155c8167bb16c88305524
nattydivi <nattydivi@yandex.ru>
2022-11-18 03:48:32 +0300

    task 8

---
+++ src/com/java/task8/Constants.java
@@ -0,0 +1,7 @@
+package com.java.task8;
+
+public class Constants {
+    public static final int DEFAULT_CAPACITY = 10;
+    public static final String ILLEGAL_INDEX = "Такого индекса не существует в массиве";
+}
+
--- src/com/java/task8/Menu.java
+++ src/com/java/task8/Menu.java
@@ -1,33 +1,68 @@
 package com.java.task8;
 
-import com.java.task7.Robot;
-
 public class Menu {
     public static void start() {
-        Robot robot = new Robot("jg");
-        Robot robot2 = new Robot("ndnm");
-        Robot robot3 = new Robot("nsnsn");
+        MyList<Integer> testMyList = new MyArrayList<>();
+        System.out.println(testMyList);
+        System.out.println("isEmpty testMyList: " + testMyList.isEmpty());
+        testMyList.add(-25);
+        testMyList.add(1);
+        testMyList.add(21);
+        testMyList.add(32);
+        testMyList.add(14);
+        testMyList.add(32);
+        testMyList.add(8);
+        testMyList.add(32);
+        testMyList.add(4);
+        testMyList.add(22);
+        testMyList.add(28);
+        testMyList.add(32);
+        testMyList.set(13, -32);
+        testMyList.set(5, -32);
+        testMyList.add(6, 101);
+        System.out.println(testMyList);
+        System.out.println("isEmpty testMyList: " + testMyList.isEmpty());
+        System.out.println(testMyList.remove(10) + " удалённый элемент");
+        System.out.println(testMyList.size());
+        System.out.println(testMyList.contains(8) + " cont");
+        System.out.println(testMyList.contains(256) + " cont");
+        System.out.println(testMyList.remove(28) + " remove object");
+        System.out.println(testMyList);
+        System.out.println("get testMyList: " + testMyList.get(4));
+        System.out.println(testMyList.indexOf(8));
 
-        MyList <Robot> stringMyList = new MyArrayList<>();
-        MyList <Robot> stringMyList2 = new MyArrayList<>(12);
+        MyList<Integer> testMyList2 = new MyArrayList<>(14);
+        System.out.println(testMyList2);
+        testMyList2.add(18);
+        testMyList2.add(14);
+        testMyList2.add(10);
+        testMyList2.add(7);
+        System.out.println(testMyList2);
+        System.out.println(testMyList2.size());
 
+        MyList<String> stringMyList = new MyArrayList<>();
+        stringMyList.add("Yes!!");
+        stringMyList.add("Really");
+        stringMyList.add("I did it");
+        stringMyList.add("I learn");
+        stringMyList.add("I ...");
+        System.out.println(stringMyList.remove("I did it") + " remove object");
         System.out.println(stringMyList);
-        System.out.println(stringMyList2);
 
+        System.out.println("get stringMyList: " + stringMyList.get(2));
+        System.out.println(stringMyList.contains("Really") + " cont");
 
+        MyList<String> stringMyListNew = new MyArrayList<>(stringMyList);
+        System.out.println(stringMyListNew.contains("Really") + " really");
+        stringMyListNew.add("Новая коллекция на основе предыдущей");
+        System.out.println(stringMyListNew);
 
-        stringMyList2.add(robot);
-        stringMyList2.add(robot2);
-
-
-//        System.out.println(myArrayList1.length);
-
+        MyList<String> stringMyList3 = new MyArrayList<>();
+        stringMyList3.add("Начало колл3");
+        stringMyList3.add("элемент");
+        System.out.println(stringMyList3);
+        stringMyList3.addAll(stringMyListNew);
+        System.out.println(stringMyList3 + "добавили колл на колл");
     }
-
-
-//    private void print(value) {
-//        System.out.println(value);
-//    }
-
 }
 
